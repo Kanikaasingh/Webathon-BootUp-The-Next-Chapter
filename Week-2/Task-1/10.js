3a41e499b98cec4f1070d187bea3e143123dd02a
Kanikaasingh <iamkanikasingh04@gmail.com>
2025-02-10 07:45:04 +0530

    Coded

--- Week-2/Task-1/10.js
+++ Week-2/Task-1/10.js
@@ -1,11 +1,11 @@
 // Q10). Printing Pattern
-let i=0, j=0;
-for(i=1;i<=5;i++)
+let i=0;
+for(i=1;i<=5;i++) //Loop for number of rows
 {
-    let r="";
-    for(j=1;j<=i;j++)
+    let r=""; 
+    for(j=1;j<=i;j++) //Loop for number of columns
     {
-        r=r+j+" ";
+        r=r+j+" "; //variable to print pattern
     }
-    console.log(r);
+    console.log(r); //Patterns printing
 }
--- Week-2/Task-1/12.js
+++ Week-2/Task-1/12.js
@@ -1,7 +1,8 @@
 // Q12). Write a function that prints the multiplication table of a number using a while loop.
-let n=prompt("Enter Number");
-let i=1;
-while(i!=11){
+let n=prompt("Enter Number"); //Accepting number from user of the number whose multiplication table is to be printed
+let i=1; 
+while(i!=11) //Printing multiplication table of the number from 1 to 10
+{
     console.log(`${n} x ${i} =`, n*i);
-    i++;
+    i++; 
 }
