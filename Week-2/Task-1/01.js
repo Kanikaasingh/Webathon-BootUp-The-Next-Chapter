f0ecd21f59c74eed2a2dd8228aa161345218ab23
Kanikaasingh <iamkanikasingh04@gmail.com>
2025-02-10 07:40:37 +0530

    Coded

--- Week-2/Task-1/01.js
+++ Week-2/Task-1/01.js
@@ -1,12 +1,10 @@
 // Q1). Write a function tha takes an input of item prices and returns the total bill
-let n=prompt("Enter number of Items");
-let i=0;
-let p=0;
-let bill=0;
-for(i=0;i<n;i++)
+let n=prompt("Enter number of Items"); //Number of items
+let i=0, p=0, bill=0; //Varibale initialization
+for(i=1;i<=n;i++)
 {
-    p=prompt(`Enter price of ${i} items`);
+    p=prompt(`Enter price of ${i} items`); //Accepting price for each item
+    p=parseInt(p); //Converting entered number to integer
+    bill=bill+p; //Adding prices
 }
-
-bill=p.reduce((pre,curr));
-console.log(`Price of ${n} items is `+bill);
+console.log(`Price of ${n} items is `+bill); //Printing total bill
